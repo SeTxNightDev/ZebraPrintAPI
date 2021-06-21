2739c69d1060317c1f8f6b7126fa9b59048186d7
SeTxNightDev <59096885+SeTxNightDev@users.noreply.github.com>
2021-06-21 10:53:06 +0200

    Update index.php

--- index.php
+++ index.php
@@ -3,6 +3,11 @@
 require 'Print.php';
 
 $h = new ZebraPrint("145.151.22.26", "9100", "^XA^FO100,100^ADN,18,10^FDTestdruck^FS^XZ")
+  
+  // ZebraPrint needs 3 values!:
+  //    first: IP-Adress of the Printer
+  //    second: Port to open socket (Standard: 9100)
+  //    third: ZPL Code String
 
 
 ?>
